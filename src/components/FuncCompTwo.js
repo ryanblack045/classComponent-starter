32d7adab1ea953fb16c33afe93db9e56859fa2b2
Ryan Black <ryanblack045@gmail.com>
2021-07-01 10:38:37 -0400

    added header for 2nd comp

--- src/components/FuncCompTwo.js
+++ src/components/FuncCompTwo.js
@@ -1,7 +1,7 @@
 const FuncCompTwo = (props) => {
   return (
     <>
-    <div>Function Component two</div>
+    <h2>Function Component two</h2>
     <div className='two'>
       <h2 className='count'>Count: {props.count}</h2>
       <h2 className='message'>Message: {props.message}</h2>
